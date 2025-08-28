c47428f2b6437275ff9cd7fe968cd2f506e2a11f
ViniciusMat0s <vinibmatos@rede.ulbra.br>
2025-08-28 20:39:36 -0300

    1

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const char = createKnight('Knight')
+const monster = createLittleMonster();
+
+stage.start (
+    char,
+    monster,
+    document.querySelector('#char'),
+    document.querySelector('#monster')
+)
